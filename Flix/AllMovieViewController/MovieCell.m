6a9f89361ee0d38d090f3e4d127e00674942247f
Michael <realq86@gmail.com>
2018-06-04 20:16:36 -0700

    ImageView fade in and load small before large

--- Flix/AllMovieViewController/MovieCell.m
+++ Flix/AllMovieViewController/MovieCell.m
@@ -7,6 +7,7 @@
 //
 
 #import "MovieCell.h"
+#import "UIKit+AFNetworking.h"
 
 @implementation MovieCell
 
@@ -19,6 +20,39 @@
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+
+- (void)setMovie:(NSDictionary *)movie {
+    _movie = movie;
+
+    NSString *urlString = [NSString stringWithFormat:@"https://image.tmdb.org/t/p/w500/%@", self.movie[@"poster_path"]];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+    __weak MovieCell *weakSelf = self;
+    [self.movieImage setImageWithURLRequest:request placeholderImage:nil
+                                    success:^(NSURLRequest *imageRequest, NSHTTPURLResponse *imageResponse, UIImage *image) {
+                                        
+                                        // imageResponse will be nil if the image is cached
+                                        if (imageResponse) {
+                                            NSLog(@"Image was NOT cached, fade in image");
+                                            weakSelf.movieImage.alpha = 0.0;
+                                            weakSelf.movieImage.image = image;
+                                            
+                                            //Animate UIImageView back to alpha 1 over 0.3sec
+                                            [UIView animateWithDuration:0.3 animations:^{
+                                                weakSelf.movieImage.alpha = 1.0;
+                                            }];
+                                        }
+                                        else {
+                                            NSLog(@"Image was cached so just update the image");
+                                            weakSelf.movieImage.image = image;
+                                        }
+                                    }
+                                    failure:^(NSURLRequest *request, NSHTTPURLResponse * response, NSError *error) {
+                                        // do something for the failure condition
+                                    }];
 }
 
 @end
--- Flix/DetailViewController.m
+++ Flix/DetailViewController.m
@@ -8,12 +8,15 @@
 
 #import "DetailViewController.h"
 #import "WebViewController.h"
+#import "UIKit+AFNetworking.h"
 
 @interface DetailViewController ()
 
 @end
 
 @implementation DetailViewController
+
+#pragma mark #2 Code snippet for : https://guides.codepath.com/ios/Working-with-UIImageView#fading-in-an-image-loaded-from-the-network
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -22,7 +25,57 @@
     
     self.movieTitle.text = self.movie[@"title"];
     self.movieDescription.text = self.movie[@"overview"];
+    
+    NSURL *url = [NSURL URLWithString:@"https://image.tmdb.org/t/p/w500/j0BtDE8M4Q2sJANrQjCosU8N7ji.jpg"];
+    
+//    [self.imageView setImageWithURL:url];
+#pragma mark #2 END
+    
+#pragma mark #3 Code snippet for : https://github.com/codepath/ios_guides/wiki/Working-with-UIImageView
+    NSURL *urlSmall = [NSURL URLWithString:@"https://image.tmdb.org/t/p/w200/j0BtDE8M4Q2sJANrQjCosU8N7ji.jpg"];
+    NSURL *urlLarge = [NSURL URLWithString:@"https://image.tmdb.org/t/p/w500/j0BtDE8M4Q2sJANrQjCosU8N7ji.jpg"];
+
+    NSURLRequest *requestSmall = [NSURLRequest requestWithURL:urlSmall];
+    NSURLRequest *requestLarge = [NSURLRequest requestWithURL:urlLarge];
+
+    __weak DetailViewController *weakSelf = self;
+
+    [self.imageView setImageWithURLRequest:requestSmall
+                          placeholderImage:nil
+                                   success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *smallImage) {
+                                       
+                                       // smallImageResponse will be nil if the smallImage is already available
+                                       // in cache (might want to do something smarter in that case).
+                                       weakSelf.imageView.alpha = 0.0;
+                                       weakSelf.imageView.image = smallImage;
+                                       
+                                       [UIView animateWithDuration:0.3
+                                                        animations:^{
+                                                            
+                                                            weakSelf.imageView.alpha = 1.0;
+                                                            
+                                                        } completion:^(BOOL finished) {
+                                                            // The AFNetworking ImageView Category only allows one request to be sent at a time
+                                                            // per ImageView. This code must be in the completion block.
+                                                            [weakSelf.imageView setImageWithURLRequest:requestLarge
+                                                                                  placeholderImage:smallImage
+                                                                                           success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage * largeImage) {
+                                                                                                weakSelf.imageView.image = largeImage;
+                                                                                  }
+                                                                                           failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
+                                                                                               // do something for the failure condition of the large image request
+                                                                                               // possibly setting the ImageView's image to a default image
+                                                                                           }];
+                                                        }];
+                                   }
+                                   failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
+                                       // do something for the failure condition
+                                       // possibly try to get the large image
+                                   }];
+    
+#pragma mark #3 END
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
